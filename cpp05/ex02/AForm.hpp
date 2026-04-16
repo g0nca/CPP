4cd34daa286e96de33a638a97280f60090f36a84
Gonca <ggomes-v@c2r2s3.42porto.com>
2026-04-16 12:05:46 +0100

    AForm.hpp from ex02 modified

--- cpp05/ex02/AForm.hpp
+++ cpp05/ex02/AForm.hpp
@@ -55,16 +55,22 @@ class AForm
                 }
         };
 
+        class FormCreationException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw()
+                {
+                    return ("Form creation failed");
+                }
+        }
+
         class FileNotOpenedException : public std::exception
         {
             public:
-                virtual const char* what() const throw();
-        };
-        
-        class FormNotSignedException : public std::exception
-        {
-            public:
-                virtual const char* what() const throw();
+                virtual const char* what() const throw()
+                {
+                    return ("Could not open file");
+                }
         };
 
 };
