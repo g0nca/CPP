71b0b611d403a8c3cf07af9db464931706cec874
g0nca <vgncalo63@gmail.com>
2026-04-27 14:43:03 +0100

    ex01 done

--- cpp06/ex01/Serializer.hpp
+++ cpp06/ex01/Serializer.hpp
@@ -1,9 +1,21 @@
+#pragma once
 
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <stdint.h>
+#include "Data.hpp"
 
 class   Serializer
 {
     private:
+        Serializer(void);
+        Serializer(Serializer& );
+        Serializer& operator=(Serializer& copy);
+        ~Serializer();
 
     public:
-
+        static uintptr_t serialize(Data* ptr);
+        static Data* deserialize(uintptr_t raw);
 };
--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -1,7 +1,25 @@
+#include "Serializer.hpp"
 
-int     main(int ac, char **av)
-{
-    (void)ac, (void)av;
+int	main( int argc , char *argv[] ) {
+	(--argc, ++argv);
+	if (argc != 2) {
+		std::cout << "Correct usage: ./exec <string> <number>" << std::endl;
+		return (1);
+	}
 
-    return (0);
+	std::string	name = argv[0], number = argv[1];
+	if (name.empty() || number.empty() || number.length() > 5)
+		return (1);
+
+	Data		data = (Data){name, strtol(number.c_str(), NULL, 10)};
+
+	uintptr_t	s = Serializer::serialize(&data);
+
+	std::cout << "Original address  => " << &data << std::endl;
+	std::cout << "Serialized (dec)  => " << s << std::endl;
+	std::cout << "Serialized (hex)  => 0x" << std::hex << s << std::dec << std::endl;
+	std::cout << "Deserialized ptr  => " << Serializer::deserialize(s) << std::endl;
+
+	std::cout << "Data [" << data.name << ", " << data.value << "]" << std::endl;
+	return (0);
 }
