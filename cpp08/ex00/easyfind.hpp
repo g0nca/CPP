7bd3cd5db1276654bd9c6c8d20e0ace0186f2684
g0nca <vgncalo63@gmail.com>
2026-05-04 16:31:18 +0100

    Exercise 00 of CPP08 Done

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <exception>
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+#include <algorithm>
+
+template <typename T>
+typename T::iterator    easyfind(T& container, int value)
+{
+    typename T::iterator it;
+
+    it = std::find(container.begin(), container.end(), value);
+    if (it == container.end())
+        throw std::exception();
+    
+    return (it);
+};
+
---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,30 @@
+#include "easyfind.hpp"
+
+int	main( void ) {
+	std::vector<int> n;
+	n.push_back(0);
+	n.push_back(5);
+	n.push_back(4);
+	n.push_back(3);
+	n.push_back(2);
+	n.push_back(1);
+    
+
+	try {
+		std::cout << *(::easyfind(n, 5)) << std::endl;
+		std::cout << *(::easyfind(n, 2)) << std::endl;
+
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+
+	const std::vector<int> nconst(n);
+	try {
+		std::cout << *(::easyfind(n, 5)) << std::endl;
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+}
